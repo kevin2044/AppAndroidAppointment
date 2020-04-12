16699aca409d650c17a3a4317e26216b14c2c053
Kevin Brayan Luna Figueroa <kevin_2044@hotmail.com>
2020-04-12 01:06:26 -0600

    Agregando snackbar by lazy

--- app/src/main/java/mx/com/kevinlunaweb/myappointments/MainActivity.kt
+++ app/src/main/java/mx/com/kevinlunaweb/myappointments/MainActivity.kt
@@ -5,9 +5,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val snackBar by lazy {
+        Snackbar.make(mainLayout, R.string.press_back_again, Snackbar.LENGTH_LONG)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         createHere.setOnClickListener {
+            Toast.makeText(this,"Que onda",Toast.LENGTH_LONG).show()
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
@@ -43,5 +49,13 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onBackPressed() {
+        if(snackBar.isShown){
+            super.onBackPressed()
+        }else{
+            snackBar.show()
+        }
     }
 }
